b09bfe5899fee3aab95ec5a3f9bfe8f2b5f63e66
AndrewAlan <andrewalangm@gmail.com>
2019-03-07 17:32:37 -0600

    Lectura del archivo y validaciones
    * Lectura del contenido del archivo
    * Validando errores en lectura
    * Validando extension correcta

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 /* jshint esversion:6 */
 /* IMPORTS */
 const { argv } = require('./config/comando');
+const path = require('path');
+const files = require('fs');
 
-console.log(argv);
+
+/**
+ * Recibe el path del archivo para determinar si existe y es un archivo txt valido
+ * @param {string} pathArchivo 
+ */
+const calcularMinas = (pathArchivo) => {
+    // validamos el formato del archivo
+    const archivo = path.extname(pathArchivo);
+    if (archivo !== '.txt') {
+        console.log('El archivo no tiene formato permitido debe ser .txt');
+        return;
+    }
+    // leer el archvio
+    files.readFile(pathArchivo, (err, data) => {
+        if (err) {
+            console.log(`No se puedele leer el archivo`, err.code);
+            return;
+        }
+
+        let contenido = data.toString();
+    });
+};
+
+calcularMinas(argv._[0]);
--- config/comando.js
+++ config/comando.js
@@ -1,5 +1,7 @@
 /* jshint esversion:6 */
 /* IMPORTS */
+//confuguracion para el comando de ejecucion
 const argv = require('yargs').command('$0', 'Archivo de texto con el tablero a calcular').argv;
 
+// exports
 module.exports = { argv };
